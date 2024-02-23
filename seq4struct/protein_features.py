41679c413c8ce5e1f9d2a1237b715203c4793128
Karandeep Singh Nanda <karandeepsinghnanda@karandeeps-mbp.lan>
2024-02-22 19:28:53 -0700

    finished self attention pipeline

---
+++ seq4struct/protein_features.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+import copy
+
+import matplotlib.pyplot as plt
+
